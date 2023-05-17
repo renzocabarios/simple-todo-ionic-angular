0684400d3584856428882af4494bf25d553c19e6
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-05-17 10:32:36 +0800

    Add deleteTodo in TodoService

--- src/app/shared/todo.service.ts
+++ src/app/shared/todo.service.ts
@@ -10,10 +10,19 @@ export class TodoService {
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
-  url = 'https://api-simple-todo.onrender.com/api/v1/todo';
+
+  url = 'https://api-simple-todo.onrender.com/api/v1/todo/';
+
   constructor(private http: HttpClient) {}
-  getSongList(): Observable<IHttpResponse<ITodo>> {
+
+  getTodos(): Observable<IHttpResponse<ITodo>> {
     return this.http.get<IHttpResponse<ITodo>>(this.url).pipe();
+  }
+
+  deleteTodo(id: string): Observable<IHttpResponse<ITodo>> {
+    return this.http
+      .delete<IHttpResponse<ITodo>>(`${this.url}${id}`, this.httpOptions)
+      .pipe();
   }
   // addSong(song: Song): Observable<any> {
   //   return this.http
