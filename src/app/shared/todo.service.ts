f332bec87c7455d113bfac552b038ed60bdade90
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-05-17 14:45:03 +0800

    Add getTodoById in TodoService

--- src/app/shared/todo.service.ts
+++ src/app/shared/todo.service.ts
@@ -31,29 +31,9 @@ export class TodoService {
       .pipe();
   }
 
-  // updateSong(id, song: Song): Observable<any> {
-  //   return this.http
-  //     .put(
-  //       'http://localhost:3000/api/update-song/' + id,
-  //       song,
-  //       this.httpOptions
-  //     )
-  //     .pipe(
-  //       tap((_) => console.log(`Song updated: ${id}`)),
-  //       catchError(this.handleError<Song[]>('Update Song'))
-  //     );
-  // }
-  // deleteSong(id): Observable<Song[]> {
-  //   return this.http
-  //     .delete<Song[]>(
-  //       'http://localhost:3000/api/delete-song/' + id,
-  //       this.httpOptions
-  //     )
-  //     .pipe(
-  //       tap((_) => console.log(`Song deleted: ${id}`)),
-  //       catchError(this.handleError<Song[]>('Delete Song'))
-  //     );
-  // }
+  getTodoById(id: string): Observable<IHttpResponse<ITodo>> {
+    return this.http.get<IHttpResponse<ITodo>>(`${this.url}${id}`).pipe();
+  }
 
   // private handleError<T>(operation = 'operation', result?: T) {
   //   return (error: any): Observable<T> => {
