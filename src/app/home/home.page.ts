914dc82a981adcef8a521dba780820bbae0a8877
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-05-17 10:27:21 +0800

    Update home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TodoService } from '../shared/todo.service';
+import { ITodo } from '../interfaces';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  todos: ITodo[] = [];
+  constructor(private service: TodoService) {}
 
-  constructor() {}
-
+  ngOnInit(): void {}
+  ionViewDidEnter() {
+    this.service.getSongList().subscribe((res) => {
+      const { status, data } = res;
+      if (status == 'success') {
+        this.todos = data;
+      }
+    });
+  }
 }
