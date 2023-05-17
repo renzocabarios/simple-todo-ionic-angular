c68a41b9f736a8d353f8ef43f0cbb62ebcd871b6
Renzo Cabarios <renzo.cabarios@gmail.com>
2023-05-17 14:17:19 +0800

    Add CreateTodoPage

--- src/app/create-todo/create-todo.page.ts
+++ src/app/create-todo/create-todo.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl } from '@angular/forms';
+import { TodoService } from '../shared/todo.service';
+import { ITodo } from '../interfaces';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-todo',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-todo.page.scss'],
 })
 export class CreateTodoPage implements OnInit {
+  ionicForm: FormGroup = new FormGroup({
+    content: new FormControl(''),
+  });
 
-  constructor() { }
+  constructor(
+    public formBuilder: FormBuilder,
+    private service: TodoService,
+    private router: Router
+  ) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  submit(): void {
+    const temp: ITodo = this.ionicForm.value;
+    this.service.addTodo(temp).subscribe((res) => {
+      const { status, message } = res;
+      if (status == 'success') {
+        this.ionicForm.reset();
+        this.router.navigate(['/home']);
+      }
+    });
   }
-
 }
